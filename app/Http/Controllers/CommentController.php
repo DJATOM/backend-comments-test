b1b099c8e2bafef4184b01668003dc3f257e0add
DJATOM <djatom@beatrice-raws.org>
2022-01-27 23:47:23 +0200

    Implement basic CRUD on Comment

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Comment\StoreRequest;
+use App\Http\Requests\Comment\UpdateRequest;
+use App\Http\Resources\CommentResource;
+use App\Models\Comment;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the comments.
+     *
+     * @return ResourceCollection
+     */
+    public function index(): ResourceCollection
+    {
+        $comments = Comment::root()
+            ->latest()
+            ->get();
+
+        return CommentResource::collection($comments);
+    }
+
+    /**
+     * Store a newly created comment in storage.
+     *
+     * @param  StoreRequest  $request
+     * @return CommentResource
+     */
+    public function store(StoreRequest $request): CommentResource
+    {
+        $comment = Comment::create($request->validated());
+
+        return new CommentResource($comment);
+    }
+
+    /**
+     * Display the specified comment.
+     *
+     * @param  Comment  $comment
+     * @return CommentResource
+     */
+    public function show(Comment $comment): CommentResource
+    {
+        return new CommentResource($comment);
+    }
+
+    /**
+     * Update the specified comment in storage.
+     *
+     * @param  UpdateRequest  $request
+     * @param  Comment  $comment
+     * @return CommentResource
+     */
+    public function update(UpdateRequest $request, Comment $comment): CommentResource
+    {
+        $comment->update($request->validated());
+
+        return new CommentResource($comment);
+    }
+
+    /**
+     * Remove the specified comment from storage.
+     *
+     * @param  Comment  $comment
+     * @return Response
+     */
+    public function destroy(Comment $comment): Response
+    {
+        $comment->delete();
+
+        return response()->json(null, 204);
+    }
+}
